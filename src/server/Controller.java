78ffe13e3e302979a063d1386b6b4ef8dc1494c9
Łukasz Szcześniak <sir.viters@gmail.com>
2017-02-19 16:14:06 +0100

    This classes don't have implementation, so they can be abstract

--- src/server/Controller.java
+++ src/server/Controller.java
@@ -3,6 +3,6 @@ package server;
 /**
  * Created by Viters on 10.01.2017.
  */
-public class Controller {
+public abstract class Controller {
 
 }
--- src/server/State.java
+++ src/server/State.java
@@ -3,6 +3,6 @@ package server;
 /**
  * Created by Viters on 11.01.2017.
  */
-public class State {
+public abstract class State {
 
 }
