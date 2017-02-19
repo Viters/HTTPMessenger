32fb162c17cfa4e2218fd8bcf71c9e567cf62d25
Łukasz Szcześniak <sir.viters@gmail.com>
2017-02-19 20:45:18 +0100

    Response class test for later refactoring

---
+++ src/server/tests/ResponseTest.java
@@ -0,0 +1,20 @@
+import org.junit.jupiter.api.Test;
+import server.Response;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class ResponseTest {
+    @Test
+    void formatResponseAsStringValidForSending() {
+        // given
+        Response response = new Response();
+        response.appendConnection("close")
+                .appendBody("Hello!");
+
+        // when
+        String responseToBeSent = response.make();
+
+        // then
+        assertEquals("Connection: close\r\n\r\nHello!", responseToBeSent);
+    }
+}
