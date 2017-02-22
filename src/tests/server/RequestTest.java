f9ae37e1f6f32f386b5eed347462821c89e7f921
Viters <viters266@gmail.com>
2017-02-22 15:37:53 +0100

    Tests that allow me to refactor request method handling

--- src/tests/server/RequestTest.java
+++ src/tests/server/RequestTest.java
@@ -1,7 +1,60 @@
 package server;
 
-import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.StringReader;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 class RequestTest {
+    private static final String exampleGetRequest;
+    private static final String examplePostRequest;
+    private static final String exampleOptionsRequest;
 
+    static {
+        exampleGetRequest = "GET http://www.w3.org/pub/WWW/TheProject.html HTTP/1.1";
+        examplePostRequest = "";
+        exampleOptionsRequest = "";
+    }
+
+    @Test
+    void checkIfGetMethodIsCorrectlyAssigned() throws IOException {
+        // given
+        StringReader clientInput = new StringReader(exampleGetRequest);
+
+        // when
+        Request request = new Request(new BufferedReader(clientInput));
+        boolean[] methodCheck = {request.isGet(), request.isPost(), request.isOption()};
+
+        // then
+        assertThat(methodCheck).isEqualTo(new boolean[]{true, false, false});
+    }
+
+    @Test
+    void checkIfPostMethodIsCorrectlyAssigned() throws IOException {
+        // given
+        StringReader clientInput = new StringReader(examplePostRequest);
+
+        // when
+        Request request = new Request(new BufferedReader(clientInput));
+        boolean[] methodCheck = {request.isGet(), request.isPost(), request.isOption()};
+
+        // then
+        assertThat(methodCheck).isEqualTo(new boolean[]{false, true, false});
+    }
+
+    @Test
+    void checkIfOptionsMethodIsCorrectlyAssigned() throws IOException {
+        // given
+        StringReader clientInput = new StringReader(exampleOptionsRequest);
+
+        // when
+        Request request = new Request(new BufferedReader(clientInput));
+        boolean[] methodCheck = {request.isGet(), request.isPost(), request.isOption()};
+
+        // then
+        assertThat(methodCheck).isEqualTo(new boolean[]{false, false, true});
+    }
 }
